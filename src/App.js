72632121746603fb450b268e4fae6568162b5e55
sappy12dream <sapnaspy@gmail.com>
2022-02-21 17:15:20 +0530

    deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles.scss'
+import {BsFillDice5Fill, BsPauseFill} from 'react-icons/bs'
+import {  useState } from 'react'
+import axios from 'axios'
 
 function App() {
+  const [slip, setslip] = useState({id:'00',advice:'blank'})
+
+  async function getAdvice() {
+  try {
+    const response = await axios.get('https://api.adviceslip.com/advice');
+    setslip(response.data.slip)
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+  
+    
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='card'>
+        <span>ADVICE #{slip.id}</span>
+        <p>"{slip.advice}"</p>
+        <div className='play'>
+          <div className='line'></div>
+          <div className='playbtn'><BsPauseFill/></div>
+          <div className='line'></div>
+        </div>
+        <div className='logo' onClick = {getAdvice} >
+          <div><BsFillDice5Fill/></div>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
+export default App
